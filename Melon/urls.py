8937f07342d4527d7319379ebbcd67d748f2d1f9
kalu <maticluka888@gmail.com>
2024-12-01 23:20:30 +0100

    statistics

--- Melon/urls.py
+++ Melon/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('clan/', views.clan, name="clan"),
     path('vesti/', views.vesti, name="vesti"),
     path('login/', views.loginPage, name="login"),
-
+    path('store-transitions/<str:store_name>/', views.store_transitions, name='store_transitions'),
 ]
 
 #if settings.DEBUG:
